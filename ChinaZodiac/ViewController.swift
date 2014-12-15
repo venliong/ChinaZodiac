410c8a54171fdf23a349caa277e912f967f9512c
lidashuang <ldshuang@gmail.com>
2014-12-15 19:18:49 +0800

    step 1

--- ChinaZodiac/ViewController.swift
+++ ChinaZodiac/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var yearOfBirth: UITextField!
+    
+    @IBOutlet weak var image: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func okTapped(sender: AnyObject) {
+        var year = yearOfBirth.text.toInt()
+    }
 
 }
 
