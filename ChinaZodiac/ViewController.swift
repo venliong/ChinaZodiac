e7637a0342f569793702de5d621b39da29a934e0
lidashuang <ldshuang@gmail.com>
2014-12-15 22:48:52 +0800

    step 2

--- ChinaZodiac/ViewController.swift
+++ ChinaZodiac/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var image: UIImageView!
     
+    let offset = 4
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,9 +25,19 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    override func touchesEnded(touches: NSSet, withEvent event: UIEvent) {
+        yearOfBirth.resignFirstResponder()
+    }
+    
     @IBAction func okTapped(sender: AnyObject) {
-        var year = yearOfBirth.text.toInt()
+        yearOfBirth.resignFirstResponder()
+        if let year = yearOfBirth.text.toInt() {
+            var imageNumber = (year - offset) % 12
+            image.image = UIImage(named: String(imageNumber))
+        } else {
+            
+        }
     }
 
 }
